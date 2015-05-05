dc97e2c80a8e197c67efd7fef93e1e66a01ac1c2
Kleison Schuelter <kleison.schuelter@texoit.com>
2015-05-05 17:37:16 -0300

    Added support for rest services

---
+++ src/main/java/com/texoit/website/config/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.texoit.website.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest/protected/t3x01trul3z")
+public class JaxRsActivator extends Application {
+	/* class body intentionally left blank */
+}
---
+++ src/main/java/com/texoit/website/rest/EmailService.java
@@ -0,0 +1,9 @@
+package com.texoit.website.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+@Path("/emails")
+@RequestScoped
+public class EmailService {
+}
