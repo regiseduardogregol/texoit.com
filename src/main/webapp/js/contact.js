b9802146a1b2998f499ae08e0b4bfd1e863d477e
André Filippi Cardoso Farias <andre.cardoso@texoit.com>
2015-05-12 20:42:15 -0300

    Added mask to the phone field in the contact form.

--- src/main/webapp/js/contact.js
+++ src/main/webapp/js/contact.js
@@ -1,3 +1,17 @@
+$("#phone").mask("(99) 99999-9999");
+$("#phone").on("blur", function() {
+    var last = $(this).val().substr( $(this).val().indexOf("-") + 1 );
+    
+    if( last.length == 3 ) {
+        var move = $(this).val().substr( $(this).val().indexOf("-") - 1, 1 );
+        var lastfour = move + last;
+        
+        var first = $(this).val().substr( 0, 9 );
+        
+        $(this).val( first + '-' + lastfour );
+    }
+});
+
 $('#contactForm').submit(function(event) {
 	$('#contactSubmit').prop("disabled", true);
 	$('#contactError').fadeOut(250);
